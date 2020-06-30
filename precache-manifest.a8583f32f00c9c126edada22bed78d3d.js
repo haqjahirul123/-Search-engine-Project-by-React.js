4893fafb1fbef0fded1447dfbb1ab8c7af610f60
haqjahirul123 <haqjahirul1983@gmail.com>
2020-06-30 14:32:09 +0200

    Updates

---
+++ precache-manifest.a8583f32f00c9c126edada22bed78d3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4dc501de22380bfe8c0b288596773b",
+    "url": "/reactApp/index.html"
+  },
+  {
+    "revision": "99359a556854863a652a",
+    "url": "/reactApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f5f99ac37af0ab8b1d4c",
+    "url": "/reactApp/static/js/2.09d8112b.chunk.js"
+  },
+  {
+    "revision": "4275c1d12d633733c6fec0905104cb9a",
+    "url": "/reactApp/static/js/2.09d8112b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99359a556854863a652a",
+    "url": "/reactApp/static/js/main.f3807394.chunk.js"
+  },
+  {
+    "revision": "389a838f219fe2679cf3",
+    "url": "/reactApp/static/js/runtime-main.30da069e.js"
+  }
+]);
